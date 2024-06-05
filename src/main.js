5ceb304e45d0920c92fb2242cb78658afc001a2f
Luan Costa <lnmaiscedo@gmail.com>
2024-06-05 18:47:36 -0300

    Adicionando o header e o arquivo js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,19 @@ document.addEventListener('DOMContentLoaded', function(){
         const posicaoAtual = window.scrollY;
     
         if (posicaoAtual < alturaHero){
-    
+            ocultaHeader();
+        } else {
+            exibeHeader();
         }
     })
 })
+
+function ocultaHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
